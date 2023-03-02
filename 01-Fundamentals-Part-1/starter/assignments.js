eb9e784bbad05cfa6eb86034ea494ac9c7b244ad
IgorPinchuk <igor_pig@mail.ru>
2023-03-02 21:58:19 +0300

    Assignments JS Fundamentals - Part 1 - let, const and var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -2,8 +2,8 @@
 1. Declare variables called 'country', 'continent' and 'population' and 
 assign their values according to your own country (population in millions)
 2. Log their values to the console */
-let country = 'Belarus';
-let continent = 'Eurasia';
+const country = 'Belarus';
+const continent = 'Eurasia';
 let population = 10;
 
 console.log(country);
@@ -16,10 +16,19 @@ country. The variable should hold a Boolean value. Also declare a variable
 'language', but don't assign it any value yet
 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 to the console */
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+/* LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some 
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never 
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens */
+language = 'Belarusian';
+//isIsland = true;
