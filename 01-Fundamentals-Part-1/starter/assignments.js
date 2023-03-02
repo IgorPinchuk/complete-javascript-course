f2689cc1183dadb908c6cd00e149ed7cb04310f5
IgorPinchuk <igor_pig@mail.ru>
2023-03-02 21:32:10 +0300

    Assignments JS Fundamentals - Part 1 - Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -9,3 +9,17 @@ let population = 10;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+/* LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
