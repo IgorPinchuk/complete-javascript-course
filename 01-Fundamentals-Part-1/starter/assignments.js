828cd475fd70acb46b5660ea95c6d63f81b6b952
IgorPinchuk <igor_pig@mail.ru>
2023-03-03 14:07:42 +0300

    Assignments JS Fundamentals - Part 1 - Strings and Template Literals

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -48,5 +48,11 @@ console.log(population / 2);
 console.log(++population); 
 console.log(population > 6); 
 console.log(population < 33);
-const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description);
+
+/* LECTURE: Strings and Template Literals
+1. Recreate the 'description' variable from the last assignment, this time 
+using the template literal syntax */
+description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 console.log(description);
