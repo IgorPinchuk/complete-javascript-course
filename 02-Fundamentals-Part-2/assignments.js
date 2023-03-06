f1e7b3d46f2852b00ae1d68cf65dd34db4923b96
IgorPinchuk <igor_pig@mail.ru>
2023-03-06 20:50:14 +0300

    Assignments JS Fundamentals - Part 2 - Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -15,3 +15,25 @@ const china = describeCountry('China', 1412, 'Beijing');
 console.log(belarus);
 console.log(germany);
 console.log(china);
+
+/* LECTURE: Function Declarations vs. Expressions
+1. The world population is 7900 million people. Create a function declaration
+called 'percentageOfWorld1' which receives a 'population' value, and
+returns the percentage of the world population that the given population
+represents. For example, China has 1441 million people, so it's about 18.2% of
+the world population
+2. To calculate the percentage, divide the given 'population' value by 7900
+and then multiply by 100
+3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+store the results into variables, and log them to the console
+4. Create a function expression which does the exact same thing, called
+'percentageOfWorld2', and also call it with 3 country populations (can be
+the same populations) */
+function percentageOfWorld1(population){
+    return population / 7900 * 100;
+}
+const percentageOfWorld2 = function(population){
+    return population / 7900 * 100;
+}
+console.log(percentageOfWorld1(10), percentageOfWorld1(83), percentageOfWorld1(1412));
+console.log(percentageOfWorld2(10), percentageOfWorld2(83), percentageOfWorld2(1412));
