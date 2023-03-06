aa5aec501cd0106a4d16b326a738b435432bc4be
IgorPinchuk <igor_pig@mail.ru>
2023-03-06 20:15:32 +0300

    Assignments JS Fundamentals - Part 2 - Functions

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,17 @@
+'use strict';
+/* LECTURE: Functions
+1. Write a function called 'describeCountry' which takes three parameters:
+'country', 'population' and 'capitalCity'. Based on this input, the
+function returns a string with this format: 'Finland has 6 million people and its
+capital city is Helsinki'
+2. Call this function 3 times, with input data for 3 different countries. Store the
+returned values in 3 different variables, and log them to the console */
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`;    
+}
+const belarus = describeCountry('Belarus', 10, 'Minsk');
+const germany = describeCountry('Germany', 83, 'Berlin');
+const china = describeCountry('China', 1412, 'Beijing');
+console.log(belarus);
+console.log(germany);
+console.log(china);
