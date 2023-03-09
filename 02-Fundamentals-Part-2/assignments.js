f3913b8b24f5837437a08cfd938e073fdf9a1c46
IgorPinchuk <igor_pig@mail.ru>
2023-03-09 19:02:06 +0300

    Assignments JS Fundamentals - Part 2 - Functions Calling Other Functions

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -43,3 +43,16 @@ console.log(percentageOfWorld2(10), percentageOfWorld2(83), percentageOfWorld2(1
 'percentageOfWorld3' */
 const percentageOfWorld3 = population => population / 7900 * 100;
 console.log(percentageOfWorld3(10), percentageOfWorld3(83), percentageOfWorld3(1412));
+
+/* LECTURE: Functions Calling Other Functions
+1. Create a function called 'describePopulation'. Use the function type you
+like the most. This function takes in two arguments: 'country' and
+'population', and returns a string like this: 'China has 1441 million people,
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice */
+function describePopulation(country, population){
+    return `${country} has ${population} million people, which is about ${percentageOfWorld3(population)}% of the world.`
+}
+console.log(describePopulation(`Belarus`, 10));
