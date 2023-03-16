07bd07c695f5e15d3505afd603dec00f6f9d7cab
IgorPinchuk <igor_pig@mail.ru>
2023-03-16 15:00:59 +0300

    Assignments JS Fundamentals - Part 2 - Iteration: The for Loop

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -139,3 +139,11 @@ myCountry.checkIsland = function(){
     this.isIsland = this.neighbours && this.neighbours.length > 0 ? false : true;
     return this.isIsland;
 };
+
+/* LECTURE: Iteration: The for Loop
+1. There are elections in your country! In a small town, there are only 50 voters.
+Use a for loop to simulate the 50 people voting, by logging a string like this to
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting' */
+for(let i = 1; i <= 50; i++){
+    console.log(`Voter number ${i} is currently voting`)
+}
